318b44d40424ffef7e7a96171867526147135b26
Tony Farha <toni.farha93@gmail.com>
2024-11-05 20:06:44 +0100

    add socket on server and client. initiate the connection to update the clipboard on multiple clients

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,27 @@
-// Todo
+import { Server } from "socket.io";
+
+const io = new Server(process.env.SOCKET_IO_PORT, {
+    cors: {
+        origin: "*"
+    }
+});
+
+io.on('connection', (socket) => {
+
+    console.log('a user connected!');
+
+    socket.on('join-clipboard', (clipboardID) => {
+        console.log(`join-clipboard: ${clipboardID}`);
+        socket.join(clipboardID);
+    });
+
+    socket.on('update-clipboard', ({ clipboardText, clipboardID }) => {
+        console.log(`update-clipboard: ${clipboardText}`);
+        io.sockets.in(clipboardID).emit('update-clipboard', clipboardText);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('a user disconnect!');
+    });
+
+});
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,13 +1,46 @@
 import { Textarea } from "@/components/ui/textarea";
 import { QRCodeSVG as QRCode } from 'qrcode.react';
+import { socket } from '@/socket';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useDebouncedCallback } from 'use-debounce';
 
 export const Home = () => {
+
+  const [clipboard, setClipboard] = useState<string>('');
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    socket.connect();
+    socket.emit('join-clipboard', id);
+
+    socket.on('update-clipboard', (clipboardText: string) => {
+      setClipboard(clipboardText);
+    });
+
+    return () => {
+      socket.off('join-clipboard');
+      socket.off('update-clipboard');
+      socket.disconnect();
+    }
+  }, []);
+
+  const handleUpdateClipboard = useDebouncedCallback((clipboardText: string) => {
+    socket.emit('update-clipboard', { clipboardText, clipboardID: id });
+  }, 500);
+
   return (
     <>
       <Textarea
         placeholder="Enter your text here..."
         className="min-h-[100px]"
         aria-label="Text input"
+        value={clipboard}
+        onChange={(e) => {
+          setClipboard(e.target.value);
+          handleUpdateClipboard(e.target.value);
+        }}
       />
       <QRCode value={`${document.location}`} />
     </>
